821bc4b7e0472aaa2ac552d8da00ed7814579bc2
Arron Schaar <aschaar@gmail.com>
2013-03-19 14:01:43 -0700

    Release 0.1.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,29 @@ module.exports = function(grunt) {
     concat: {
       options: {
         separator: '\n',
+      },
+      'x-tag-js': {
+        src: [
+          'components/document.register/src/document.register.js', 
+          'components/x-tag-core/src/core.js',
+        ],
+        dest: 'demo/x-tag-components.js'
       }
+    }, 
+    bumpup: ['component.json', 'package.json', 'xtag.json'],
+    tagrelease: {
+      file: 'package.json',
+      prefix: 'xtag-v',
+      commit: true
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
-  
+  grunt.loadNpmTasks('grunt-bumpup');
+  grunt.loadNpmTasks('grunt-tagrelease');
+
+  grunt.registerTask('build', ['concat:x-tag-js']);
+  grunt.registerTask('bump:patch', ['bumpup:patch', 'tagrelease']);
+
+
 };
